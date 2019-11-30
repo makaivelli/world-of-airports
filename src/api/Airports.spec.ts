84842e61a2572d7e810ae093381b4ac6b741eead
makaivelli <adam.makai@gmail.com>
2019-11-30 16:57:00 +0100

    Set up AirportsApi test

---
+++ src/api/Airports.spec.ts
@@ -0,0 +1,9 @@
+import AirportsApi from './Airports';
+
+const airportsApi = new AirportsApi();
+
+describe('Airports API', () => {
+    it('test should work', () => {
+        expect(true).toEqual(true);
+    })
+});
