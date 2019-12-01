1b0e02b2dc5048d997371bc058482941da8148c2
makaivelli <adam.makai@gmail.com>
2019-12-01 15:55:40 +0100

    Add cloudant client

--- src/api/Airports.ts
+++ src/api/Airports.ts
@@ -1,3 +1,6 @@
+const Cloudant = require('@cloudant/cloudant');
+const cloudant = Cloudant({ url: 'https://mikerhodes.cloudant.com'});
+const airportsDB = cloudant.db.use('airportdb');
 export default class AirportsApi {
     search() {
         return []
