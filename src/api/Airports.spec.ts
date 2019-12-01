d0ff1073dc66f19f438b185a17265fc70f1cabe2
makaivelli <adam.makai@gmail.com>
2019-12-01 18:36:30 +0100

    Mock db and add search test

--- src/api/Airports.spec.ts
+++ src/api/Airports.spec.ts
@@ -1,5 +1,8 @@
 import AirportsApi from './Airports';
+import AirportDB from '../airportDB';
 import geo from '../helpers/geo';
+jest.mock('../airportDB');
+import { mocked } from 'ts-jest/utils';
 
 const airportsApi = new AirportsApi();
 
@@ -7,6 +10,10 @@ describe('Airports API', () => {
     const latitudeCentre = 50;
     const longitudeCentre = 0;
     const radius = 224;
+
+    beforeEach(() => {
+        mocked(AirportDB.findAirports).mockClear();
+    });
 
     it(`should call 'getLatitudeRange with' 'latitudeCentre'`, () => {
         geo.getLatitudeRange = jest.fn().mockImplementation(() => [48, 52]);
@@ -20,6 +27,17 @@ describe('Airports API', () => {
 
         airportsApi.search(latitudeCentre, longitudeCentre, radius);
         expect(geo.getLongitudeRange).toHaveBeenCalledWith(0, 50, 224);
+    });
+
+    it(`should search with expected params`, () => {
+        const longitudeRange = [-2, 2];
+        const latitudeRange = [48, 52];
+        geo.getLatitudeRange = jest.fn().mockImplementation(() => latitudeRange);
+        geo.getLongitudeRange = jest.fn().mockImplementation(() => longitudeRange);
+        mocked(AirportDB.findAirports).mockResolvedValue([]);
+
+        airportsApi.search(latitudeCentre, longitudeCentre, radius);
+        expect(AirportDB.findAirports).toHaveBeenCalledTimes(1);
     });
 
     // should call the db with the right params (lat, long, query format)
