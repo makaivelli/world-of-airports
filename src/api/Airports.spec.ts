c68b3330f0412cc4072d64800051e91bf79c7721
makaivelli <adam.makai@gmail.com>
2019-12-01 16:34:26 +0100

    Add latitude range test

--- src/api/Airports.spec.ts
+++ src/api/Airports.spec.ts
@@ -1,9 +1,28 @@
 import AirportsApi from './Airports';
+import geo from '../helpers/geo';
 
 const airportsApi = new AirportsApi();
 
 describe('Airports API', () => {
-    it('test should work', () => {
-        expect(true).toEqual(true);
-    })
+    it(`should call 'getLatitudeRange with' 'latitudeCentre'`, () => {
+        const latitudeCentre = 50;
+        const longitudeCentre = 0;
+        const radius = 224;
+        geo.getLatitudeRange = jest.fn().mockImplementation(() => [48, 52]);
+
+        airportsApi.search(latitudeCentre, longitudeCentre, radius);
+        expect(geo.getLatitudeRange).toHaveBeenCalledWith(50, 224);
+    });
+
+    // should generate get the long range
+    // should generate the lat range
+    // should call the db with the right params (lat, long, query format)
+    // TODO should handle pagination
+    // should call the getDistance function
+    // should call the filter distance function (boundig box > radius)
+    // should sort the airports by distance
+
+    // integration:
+    // endpoint test: TODO validation!!!
+    // where to put helpers?
 });
