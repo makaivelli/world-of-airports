b0661e6779a9e670b9227d2b8a7649a0432250fb
makaivelli <adam.makai@gmail.com>
2019-12-01 18:35:57 +0100

    Extract db search

---
+++ src/airportDB.ts
@@ -0,0 +1,19 @@
+const Cloudant = require('@cloudant/cloudant');
+const cloudant = Cloudant({ url: 'https://mikerhodes.cloudant.com'});
+const airportDB = cloudant.db.use('airportdb');
+
+// TODO class or module?
+// TODO instantiate? dbname, etc?
+export default class AirportDB {
+    static async findAirports(longitudeRange: number[], latitudeRange: number[]) {
+        console.log('findAirports');
+        const query =
+            `lon:[ ${longitudeRange[0]} TO ${longitudeRange[1]}] AND lat:[ ${latitudeRange[0]} TO ${latitudeRange[1]}]`;
+        // TODO try / catch
+        console.log('query', query);
+        const results = await airportDB.search('view1', 'geo', {q: query});
+        console.log('results', results);
+        // TODO pagination
+        return results.rows;
+    }
+}
--- src/api/Airports.ts
+++ src/api/Airports.ts
@@ -1,28 +1,30 @@
 import geo from '../helpers/geo';
 
-const Cloudant = require('@cloudant/cloudant');
-const cloudant = Cloudant({ url: 'https://mikerhodes.cloudant.com'});
-const airportsDB = cloudant.db.use('airportdb');
+import AirportDB from '../airportDB';
+//const airportDB = new AirportDB();
 
 export default class AirportsApi {
     // integer?
-    search(latitudeCentre: number, longitudeCentre: number, radius: number) {
+    async search(latitudeCentre: number, longitudeCentre: number, radius: number) {
         // TODO validate
         const latRange: number[] = geo.getLatitudeRange(latitudeCentre, radius);
         const lonRange: number[] = geo.getLongitudeRange(longitudeCentre, latitudeCentre, radius);
-        const query = ''; // TODO
-        const results = {rows: []}; // TODO airportsDB.search('view1', 'geo', {q: query});
-        // TODO handle pagination: request with bookmark if total_rows > rows
-        const airportsWithDistances = results.rows.map(airport => {
-            const { lat, lon, name } = airport.fields;
-            const distance = geo.getDistance(latitudeCentre, lat, longitudeCentre, lon);
-            return {
-                name,
-                distance
-            }
-        });
-        const airportsInRange = airportsWithDistances.filter(airport => geo.isInRadius(radius, airport.distance));
-        const sortedAirportsInRange = airportsInRange.sort((a, b) => geo.compareByDistance(a, b));
-        return sortedAirportsInRange;
+        const airports = await AirportDB.findAirports(lonRange, latRange);
+        console.log('airports', airports);
+        if (airports && airports.length) {
+            // TODO handle pagination: request with bookmark if total_rows > rows
+            const airportsWithDistances = airports.map(airport => {
+                const { lat, lon, name } = airport.fields;
+                const distance = geo.getDistance(latitudeCentre, lat, longitudeCentre, lon);
+                return {
+                    name,
+                    distance
+                }
+            });
+            const airportsInRange = airportsWithDistances.filter(airport => geo.isInRadius(radius, airport.distance));
+            const sortedAirportsInRange = airportsInRange.sort((a, b) => geo.compareByDistance(a, b));
+            return sortedAirportsInRange;
+        }
+        return airports;
     }
 }
