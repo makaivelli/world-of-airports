44178ab04e18c9b370b95ae9693c2c861afdbd20
makaivelli <adam.makai@gmail.com>
2019-12-02 20:40:06 +0000

    Catch db error

--- src/airportDB.ts
+++ src/airportDB.ts
@@ -12,8 +12,14 @@ export default class AirportDB {
             `lon:[ ${longitudeRange[0]} TO ${longitudeRange[1]}] AND lat:[ ${latitudeRange[0]} TO ${latitudeRange[1]}]`;
         // TODO try / catch
         // Handle pagination
+        // can get params with explain?
         let params = bookmark ? {q: query, bookmark} : {q: query};
-        const results = await airportDB.search('view1', 'geo', params);
-        return { ...results, latitudeRange, longitudeRange, radius };
+        try {
+            const results = await airportDB.search('view1', 'geo', params);
+            return { ...results, latitudeRange, longitudeRange, radius };
+        } catch (e) {
+            console.error(e);
+            throw new Error('Error, try again later');
+        }
     }
 }
