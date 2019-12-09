1c5749a2778fa24027709834942f9407d53bc7fd
makaivelli <adam.makai@gmail.com>
2019-12-09 18:31:30 +0100

    Update next page api

--- src/api/Airports.ts
+++ src/api/Airports.ts
@@ -3,61 +3,86 @@ import geo from '../helpers/geo';
 import AirportDB from '../airportDB';
 import { IAirportsResult, IResource } from '../types/api';
 
-export default class AirportsApi implements IResource {
-    // integer?
-    private returnSortedAirportsInRange(result, latitudeCentre, longitudeCentre, radius) {
-        if (result && result.rows && result.rows.length) {
-            const { total_rows, rows: airports, query } = result;
-            const airportsWithDistances = airports.map(airport => {
-                const { lat, lon, name } = airport.fields;
-                const distance: number = geo.getDistance(latitudeCentre, lat, longitudeCentre, lon);
+export default class AirportsApi {
+    private airportsWithDistancesInRadius(rows, latCentre, lonCentre, radius) {
+        if (rows && rows.length) {
+            const airportsWithDistances = rows.map(airport => {
+                const id = airport.id;
+                const { lat, lon, name} = airport.fields;
+                const distance: number = geo.getDistance(latCentre, lat, lonCentre, lon);
+                // id used as key in react - there are airports with the same name ('Xakan')
                 return {
+                    id,
                     name,
+                    lat,
+                    lon,
                     distance
                 }
             });
-            const airportsInRange = airportsWithDistances.filter(airport => geo.isInRadius(radius, airport.distance));
-            const sortedAirportsInRange = airportsInRange.sort((a, b) => geo.compareByDistance(a, b));
-            return sortedAirportsInRange;
-            return {
-                total_rows,
-                airports,
-                query
-            }
+            const airportsInRadius = airportsWithDistances.filter(airport => geo.isInRadius(radius, airport.distance));
+            return airportsInRadius;
         }
         return [];
     }
-    async getNextPage(bookmark: string, latRange: number[], lonRange: number[], radius: number): Promise<IAirportsResult[]> {
-        const result = await AirportDB.findAirports(latRange, lonRange, radius);
-        return this.returnSortedAirportsInRange(result, latRange[0], lonRange[0], radius);
+    async getNextPage(bookmark: string, remainingPages: number, latRange: number[], lonRange: number[], latCentre: number, lonCentre:number, radius: number) {
+        const result = await AirportDB.findAirports(latRange, lonRange, radius, bookmark);
+        const airportsWithDistancesInRadius = this.airportsWithDistancesInRadius(result.rows, latCentre, lonCentre, radius);
+        remainingPages--;
+        const query = {
+            latRange,
+            lonRange,
+            latCentre,
+            lonCentre,
+            radius
+        };
+        return {
+            airports: airportsWithDistancesInRadius,
+            pagination: {
+                query,
+                remainingPages,
+                bookmark: result.bookmark
+            }
+        }
     }
-    async search(latitudeCentre: number, longitudeCentre: number, radius: number, bookmark?: string, query?: string) {
-        // TODO validate
-        // TODO handle pagination in FE side: request with bookmark and query if total_rows > rows
-        const latOffset: number = geo.getLatitudeOffset(latitudeCentre, radius);
-        // Handle values under and over 90deg
-        const latStart: number = geo.getLatitudeStart(latitudeCentre, latOffset);
-        const latEnd: number = geo.getLatitudeEnd(latitudeCentre, latOffset);
 
-        //const latEnd: number = latitudeCentre + latOffset > 90 ? 90 : latitudeCentre + latOffset;
+    async search(latCentre: number, lonCentre: number, radius: number) {
+        const latOffset: number = geo.getLatitudeOffset(latCentre, radius);
+        // Handle values under and over 90deg
+        const latStart: number = geo.getLatitudeStart(latCentre, latOffset);
+        const latEnd: number = geo.getLatitudeEnd(latCentre, latOffset);
 
         let lonStart: number;
         let lonEnd: number;
         let lonOffset: number;
         // Handle if radius overlaps the pole. We could have a second query only for the overlapping area, but that would add extra complexity around the pagination. Since there isn't too much airport around the poles, and we're filtering the results anyway, it's easier to query all.
-        if (geo.isOverThePole(latitudeCentre, latOffset)) {
+        if (geo.isOverThePole(latCentre, latOffset)) {
             lonStart = -180;
             lonEnd = 180;
         } else {
-            lonOffset = geo.getLongitudeOffset(longitudeCentre, latitudeCentre, radius);
+            lonOffset = geo.getLongitudeOffset(lonCentre, latCentre, radius);
             // Handle values over and under 180 deg
-            lonStart = geo.getLongitudeStart(longitudeCentre, lonOffset);
-            lonEnd = geo.getLongitudeEnd(longitudeCentre, lonOffset);
+            lonStart = geo.getLongitudeStart(lonCentre, lonOffset);
+            lonEnd = geo.getLongitudeEnd(lonCentre, lonOffset);
         }
-        const result = await AirportDB.findAirports(
-            [latStart, latEnd],
-            [lonStart, lonEnd],
-            radius);
-        return this.returnSortedAirportsInRange(result, latitudeCentre, longitudeCentre, radius);
+        const latRange = [latStart, latEnd];
+        const lonRange = [lonStart, lonEnd];
+        const result = await AirportDB.findAirports(latRange, lonRange, radius);
+        const airportsWithDistancesInRadius = this.airportsWithDistancesInRadius(result.rows, latCentre, lonCentre, radius);
+        const query = {
+            latRange,
+            lonRange,
+            latCentre,
+            lonCentre,
+            radius
+        };
+        const remainingPages = Math.ceil(result.total_rows / result.rows.length) -1;
+        return {
+            airports: airportsWithDistancesInRadius,
+            pagination: {
+                query,
+                remainingPages,
+                bookmark: result.bookmark
+            }
+        }
     }
 }
