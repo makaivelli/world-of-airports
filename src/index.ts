666dd0ea6601a7ede1a33c7abc4cfc88468cd37f
makaivelli <adam.makai@gmail.com>
2019-12-09 18:55:15 +0100

    Update node to work with client

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import * as express from 'express';
-import * as bodyParser from 'body-parser';
+import * as bodyParser from 'body-parser'
+import * as cors from 'cors';
 
 import airportsRoute from './routes/airports';
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/airports', airportsRoute);
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log('server listening on port 3000')
 });
