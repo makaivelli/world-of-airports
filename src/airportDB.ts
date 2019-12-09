8cbd6881722ea145aed6254f434458b004e349a3
makaivelli <adam.makai@gmail.com>
2019-12-09 18:35:21 +0100

    Update DB wrapper

--- src/airportDB.ts
+++ src/airportDB.ts
@@ -1,6 +1,8 @@
 const Cloudant = require('@cloudant/cloudant');
 const cloudant = Cloudant({ url: 'https://mikerhodes.cloudant.com'});
 const airportDB = cloudant.db.use('airportdb');
+const DESIGN_NAME = 'view1';
+const SEARCH_NAME = 'geo';
 
 // TODO class or module?
 // TODO instantiate? dbname, etc?
@@ -10,13 +12,10 @@ export default class AirportDB {
         // radius is needed for filtering later
         const query =
             `lon:[ ${longitudeRange[0]} TO ${longitudeRange[1]}] AND lat:[ ${latitudeRange[0]} TO ${latitudeRange[1]}]`;
-        // TODO try / catch
-        // Handle pagination
-        // can get params with explain?
         let params = bookmark ? {q: query, bookmark} : {q: query};
         try {
-            const results = await airportDB.search('view1', 'geo', params);
-            return { ...results, latitudeRange, longitudeRange, radius };
+            const results = await airportDB.search(DESIGN_NAME, SEARCH_NAME, params);
+            return { ...results, query: { latitudeRange, longitudeRange, radius }};
         } catch (e) {
             console.error(e);
             throw new Error('Error, try again later');
