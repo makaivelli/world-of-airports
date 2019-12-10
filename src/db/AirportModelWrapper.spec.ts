59469e7c2c6171365902ef65d1cdc6dd074c9996
makaivelli <adam.makai@gmail.com>
2019-12-10 19:58:54 +0100

    Update AirportModelWrapper tests

--- src/db/AirportModelWrapper.spec.ts
+++ src/db/AirportModelWrapper.spec.ts
@@ -1,39 +1,74 @@
 import AirportModelWrapper from './AirportModelWrapper';
 
-describe('AirportDB', () => {
-    const mockAirportDBWrapper = {
-        search: jest.fn(function() {
-            return;
-        }),
-    };
-    const airportModelWrapper = new AirportModelWrapper(mockAirportDBWrapper);
+describe('AirportModelWrapper', () => {
+    describe('findAirports', () => {
+        const mockAirportDBWrapper = {
+            search: jest.fn(function() {
+                return;
+            }),
+        };
+        const airportModelWrapper = new AirportModelWrapper(mockAirportDBWrapper);
 
-    afterEach(() => {
-        jest.clearAllMocks();
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+
+        it('should call search with expected params', async () => {
+            const design_name = expect.any(String);
+            const search_name = expect.any(String);
+            await airportModelWrapper.findAirports([-2, 2], [-40, 50]);
+
+            expect(mockAirportDBWrapper.search).toHaveBeenCalledTimes(1);
+            expect(mockAirportDBWrapper.search).toHaveBeenCalledWith(
+                design_name,
+                search_name,
+                { q: 'lon:[ -40 TO 50] AND lat:[ -2 TO 2]' });
+
+        });
+
+        it('should call search with bookmark', async () => {
+            const design_name = expect.any(String);
+            const search_name = expect.any(String);
+            await airportModelWrapper.findAirports([-2, 2], [-40, 50], 'whatever');
+
+            expect(mockAirportDBWrapper.search).toHaveBeenCalledTimes(1);
+            expect(mockAirportDBWrapper.search).toHaveBeenCalledWith(
+                design_name,
+                search_name,
+                { q: 'lon:[ -40 TO 50] AND lat:[ -2 TO 2]', bookmark: 'whatever' })
+        });
     });
 
-    it('should call search with expected params', async () => {
-        const design_name = expect.any(String);
-        const search_name = expect.any(String);
-        await airportModelWrapper.findAirports([-2, 2], [-40, 50]);
+    describe('findAirports on success', () => {
+        it('should return the search result', async() => {
+            const mockAirportDBWrapper = {
+                search: jest.fn(function() {
+                    return Promise.resolve('success response');
+                }),
+            };
+            const airportModelWrapper = new AirportModelWrapper(mockAirportDBWrapper);
 
-        expect(mockAirportDBWrapper.search).toHaveBeenCalledTimes(1);
-        expect(mockAirportDBWrapper.search).toHaveBeenCalledWith(
-            design_name,
-            search_name,
-             { q: 'lon:[ -40 TO 50] AND lat:[ -2 TO 2]' });
-
+            const result = await airportModelWrapper.findAirports([-2, 2], [-40, 50]);
+            expect(result).toBe('success response');
+        });
     });
 
-     it('should call search with bookmark', async () => {
-        const design_name = expect.any(String);
-        const search_name = expect.any(String);
-        await airportModelWrapper.findAirports([-2, 2], [-40, 50], 'whatever');
+    describe('findAirports on fail', () => {
+        it('should throw an error with expected message', async() => {
+            const mockAirportDBWrapper = {
+                search: jest.fn(function() {
+                    return Promise.reject('error response');
+                }),
+            };
+            const airportModelWrapper = new AirportModelWrapper(mockAirportDBWrapper);
 
-        expect(mockAirportDBWrapper.search).toHaveBeenCalledTimes(1);
-        expect(mockAirportDBWrapper.search).toHaveBeenCalledWith(
-            design_name,
-            search_name,
-            { q: 'lon:[ -40 TO 50] AND lat:[ -2 TO 2]', bookmark: 'whatever' })
-     });
+            try {
+                await airportModelWrapper.findAirports([-2, 2], [-40, 50]);
+                // Force fail test
+                expect(false).toBe(true);
+            } catch (e) {
+                expect(e.message).toBe('Error');
+            }
+        });
+    });
 });
