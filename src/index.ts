93d1567f4197a72f857d8be5c8879d2f3b79edc3
makaivelli <adam.makai@gmail.com>
2019-12-10 19:17:49 +0100

    Inject instances

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,24 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser'
 import * as cors from 'cors';
 
-import airportsRoute from './routes/airports';
+import AirportDBWrapper from './db/AirportDBWrapper';
+import AirportModelWrapper from './db/AirportModelWrapper';
+import AirportsApi from './api/Airports';
+import AirportsRoute from './routes/airports';
+
+const airportDB = new AirportDBWrapper();
+const airportModelWrapper = new AirportModelWrapper(airportDB);
+const airportsApi = new AirportsApi(airportModelWrapper);
+const airportsRoute = new AirportsRoute(airportsApi);
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/airports', airportsRoute);
+// Using `post` would make the params handling a bit easier,
+// but I think using `get` for getting a collection of items makes more sense.
+app.get('/airports', airportsRoute.get);
 
 app.listen(3001, () => {
     console.log('server listening on port 3000')
