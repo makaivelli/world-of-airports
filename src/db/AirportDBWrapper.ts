19b4bd8e7379ce42ba29b3cea3c1c5b0061dfcf0
makaivelli <adam.makai@gmail.com>
2019-12-10 18:56:20 +0100

    Add airportDBWrapper

---
+++ src/db/AirportDBWrapper.ts
@@ -0,0 +1,16 @@
+import { IAirportsDBResult } from '../types';
+
+const Cloudant = require('@cloudant/cloudant');
+const cloudant = Cloudant({ url: 'https://mikerhodes.cloudant.com'});
+const airportDB = cloudant.db.use('airportdb');
+
+/**
+ * Wrapper around remote DB.
+ * Separates the actual DB access from query logic and helps unit testing.
+ */
+export default class AirportDBWrapper {
+    public search(DESIGN_NAME: string, SEARCH_NAME: string, params: object): Promise<IAirportsDBResult> {
+        console.log('not mocked', params);
+        return airportDB.search(DESIGN_NAME, SEARCH_NAME, params)
+    }
+}
