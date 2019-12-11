713235b7790129c66feff14bec2d728df3d79f8f
makaivelli <adam.makai@gmail.com>
2019-12-11 14:33:42 +0100

    Remove geo spec comments

--- server/src/helpers/geo.spec.ts
+++ server/src/helpers/geo.spec.ts
@@ -1,12 +1,3 @@
- /*
- Tests for the geo helper.
- If it weren't a coding test, I wouldn't use unit tests for the helpers.
- No need to automatically rerun the tests when the other parts of the code are changed.
- Helpers are isolated (we test the other parts of the code if they are calling them), we need to know only once if they're working as expected.
- I'm not testing here the math, only the logic
- Integration test for the math?
- */
-
 import geo from './geo';
 
 describe('geo helper', () => {
